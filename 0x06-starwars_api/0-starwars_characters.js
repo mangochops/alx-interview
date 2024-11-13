1389efe79707d96319f9248d31005c80927421b5
mangochops <133221712+mangochops@users.noreply.github.com>
2024-11-13 16:40:56 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,23 +3,45 @@ const request = require('request');
 const API_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+  const movieId = process.argv[2];
+  request(`${API_URL}/films/${movieId}/`, (err, _, body) => {
     if (err) {
-      console.log(err);
+      console.error(err);
+      process.exit(1);
     }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
+
+    let charactersURL;
+    try {
+      charactersURL = JSON.parse(body).characters;
+    } catch (parseErr) {
+      console.error('Error parsing JSON:', parseErr);
+      process.exit(1);
+    }
+
+    // Use map to create promises to fetch each character name
+    const characterNames = charactersURL.map(
       url => new Promise((resolve, reject) => {
         request(url, (promiseErr, __, charactersReqBody) => {
           if (promiseErr) {
             reject(promiseErr);
+            return;
           }
-          resolve(JSON.parse(charactersReqBody).name);
+          try {
+            const characterName = JSON.parse(charactersReqBody).name;
+            resolve(characterName);
+          } catch (parseCharErr) {
+            reject(parseCharErr);
+          }
         });
-      }));
+      })
+    );
 
-    Promise.all(charactersName)
+    // Resolve all character name promises
+    Promise.all(characterNames)
       .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
+      .catch(allErr => console.error('Error fetching character names:', allErr));
   });
+} else {
+  console.error('Please provide a movie ID as an argument');
+  process.exit(1);
 }
